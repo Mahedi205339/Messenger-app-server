a71811b022eed63d2e118b4df66eb27339af5fbc
Mahedi205339 <mahedi205339@gmail.com>
2024-08-09 11:59:54 +0600

    added mongoose and another files

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connect from "./src/db/connect.js";
 import cookieParser from "cookie-parser";
 import fs from "node:fs";
 import errorHandler from "./src/helpers/errorhandler.js";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 import User from "./src/models/auth/UserModel.js";
+import connect from "./src/db/connect.js";
 
 dotenv.config();
 
@@ -20,6 +20,8 @@ const io = new Server(httpServer, {
     origin: process.env.CLIENT_URL,
   },
 });
+
+
 
 // middleware
 app.use(
@@ -126,7 +128,7 @@ const server = async () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
-    console.log("Failed to strt server.....", error.message);
+    console.log("Failed to start server.....", error.message);
     process.exit(1);
   }
 };
--- src/db/connect.js
+++ src/db/connect.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
-    await mongoose.connect(process.env.MONGO_URI, {});
+      // console.log(process.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URL , {});
+  
     console.log("Connected to database.....");
   } catch (error) {
     console.log("Failed to connect to database.....", error.message);
